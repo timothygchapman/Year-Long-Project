4ec8a3c5638de223fed1e93832db290eac96a44c
Timothy Chapman <timothy.g.chapman98@gmail.com>
2024-03-01 12:24:06 -0700

    project finished

--- Year Long Project/ViewController.swift
+++ Year Long Project/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profilePhotoImageView: UIImageView!
+    
+    @IBOutlet weak var postProfilePhoto: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateViewBorder()
+        updateProfilePhotoCorners()
     }
-
+    
+    
+    
+    func updateProfilePhotoCorners() {
+        guard let view = profilePhotoImageView else { return }
+        
+        view.layer.cornerRadius = 8.0
+    }
+    
+    func updateViewBorder() {
+        guard let viewOne = profilePhotoImageView else { return }
+        guard let viewTwo = postProfilePhoto else { return }
+        
+        viewOne.layer.borderColor = UIColor.black.cgColor
+        viewOne.layer.borderWidth = 1.25
+        viewTwo.layer.borderColor = UIColor.black.cgColor
+        viewTwo.layer.borderWidth = 1.0
+    }
+    
 
 }
 
